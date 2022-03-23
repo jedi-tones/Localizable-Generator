258ea7ba6009e7f870638040d8f0c82aec53fa71
Denis Shchigolev <48990815+jedi-tones@users.noreply.github.com>
2022-03-24 01:16:16 +0400

    Update FileGenerator.swift

--- Sources/FileGenerator.swift
+++ Sources/FileGenerator.swift
@@ -17,7 +17,7 @@ func fileGenerator(
         if !fileManager.fileExists(atPath: directoryPath, isDirectory: &isDir) {
              try fileManager.createDirectory(atPath: directoryPath, withIntermediateDirectories: false, attributes: [:])
         }
-        let filePath = "\(directoryPath)/LocalizableTest.strings"
+        let filePath = "\(directoryPath)/Localizable.strings"
         isDir = false
         if fileManager.fileExists(atPath: filePath, isDirectory: &isDir) {
             try fileManager.removeItem(atPath: filePath)
